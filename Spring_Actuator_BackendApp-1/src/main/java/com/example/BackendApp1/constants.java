06c1a52e71384e16f64985d223f485b98cf81f21
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2020-10-04 14:38:01 +0530

    added actuator

---
+++ Spring_Actuator_BackendApp-1/src/main/java/com/example/BackendApp1/constants.java
@@ -0,0 +1,13 @@
+package com.example.BackendApp1;
+
+public class constants {
+
+    static final public String prodUrl = "abc...";
+    static final public String stageUrl = "bcd...";
+
+}
+/*
+if(env.name=="Prod"){
+
+}
+ */
---
+++ Spring_Actuator_BackendApp-1/src/test/java/com/example/BackendApp1/BackendApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
