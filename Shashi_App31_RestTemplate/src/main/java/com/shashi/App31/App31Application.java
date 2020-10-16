5c3f7299a7cdf8fc46609a199fd5e311bee20b39
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2020-10-16 22:58:01 +0530

    added codes

---
+++ Shashi_App31_RestTemplate/src/main/java/com/shashi/App31/App31Application.java
@@ -0,0 +1,18 @@
+package com.shashi.App31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App31Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(App31Application.class, args);
+
+
+
+
+	}
+
+}
---
+++ Shashi_App31_RestTemplate/src/main/java/com/shashi/App31/User.java
@@ -0,0 +1,39 @@
+package com.shashi.App31;
+
+public class User {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age>=0)
+        this.age = age;
+    }
+
+    private int id;
+    private String name;
+    private int age;
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
