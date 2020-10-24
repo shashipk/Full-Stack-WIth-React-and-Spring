d1beb08cc22ecee502dae3fcd30b833eec5da27d
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2020-10-24 22:03:40 +0530

    added code

---
+++ spring-data-jpa-one2many-join-example/src/main/java/com/javatechie/jpa/dto/OrderRequest.java
@@ -0,0 +1,20 @@
+package com.javatechie.jpa.dto;
+
+import com.javatechie.jpa.entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderRequest {
+
+    private Customer customer;
+
+    public Customer getCustomer(){
+        return this.customer;
+    }
+}
